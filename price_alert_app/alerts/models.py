0cc69232a165ed9deef1fc0a7d16683b74ecad8e
AnishAdkar <158572933+AnishAdkar@users.noreply.github.com>
2024-07-28 18:46:00 +0530

    Created Models for Alerts and Users

--- price_alert_app/alerts/models.py
+++ price_alert_app/alerts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Alert(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cryptocurrency = models.CharField(max_length=10)
+    target_price = models.DecimalField(max_digits=20, decimal_places=8)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, default='created')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.cryptocurrency} at {self.target_price}"
