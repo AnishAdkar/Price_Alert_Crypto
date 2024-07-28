af267a1663e458f5088d753254d3d68a02390e0f
AnishAdkar <158572933+AnishAdkar@users.noreply.github.com>
2024-07-28 19:04:00 +0530

    Integrated CoinGecko API to Fetch Cryptocurrency Prices

---
+++ price_alert_app/price_alert_app/celery.py
@@ -0,0 +1,19 @@
+# price_alert_app/celery.py
+
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'price_alert_app.settings')
+
+app = Celery('price_alert_app')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
